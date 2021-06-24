d5f37fa80d81195468c6fcae2781f7d1b285b5da
Denis Espanhol <denisgfespanhol@gmail.com>
2021-06-24 18:54:44 -0300

    Verifica se o resultado aritmético é maior que 20

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/programa04.js
@@ -0,0 +1,18 @@
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+
+for (let index = 0; index < numbers.length; index += 1) {
+  sum += numbers[index];
+}
+
+let arithmetic = sum / numbers.length;
+
+if (arithmetic > 20) {
+  console.log('Valor maior que 20.');
+}
+else {
+  console.log('Valor menor ou igual a 20');
+}
