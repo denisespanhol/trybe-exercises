a9efccb2472ff7e99263f020b49bf27fd2f23026
Denis Espanhol <denisgfespanhol@gmail.com>
2021-06-24 11:01:57 -0300

    Retorna a movimentação das peças de xadrez.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/programa10.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/programa10.js
@@ -1,26 +1,26 @@
 // Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
 
-let chessPiece = 'peão';
+let chessPiece = 'BiSpO';
 
-switch (chessPiece) {
-    case 'Rei':
-        console.log('Anda uma casa em qualquer direção.');
-        break;
-    case 'Rainha' || 'Dama':
-        console.log('Anda inúmeras casas em qualquer direção');
-        break;
-    case 'Torre':
-        console.log('Anda inúmeras casas em linhas retas');
-        break;
-    case 'Bispo':
-        console.log('Anda inúmeras casas em diagonais');
-        break;
-    case 'Cavalo':
-        console.log('Anda uma quantidade de casas suficiente para formar um L');
-        break;
-    case 'Peão':
-        console.log('Anda uma casa para frente; No primeiro movimento pode andar duas casas para a frente; Captura peças apenas pela posição diagonal.')
-        break;
-    default:
-        console.log('Acho que isso não é uma peça de xadrez...')
+switch (chessPiece.toLowerCase()) {
+  case 'rei':
+    console.log('Anda uma casa em qualquer direção.');
+    break;
+  case 'rainha' || 'dama':
+    console.log('Anda inúmeras casas em qualquer direção');
+    break;
+  case 'torre':
+    console.log('Anda inúmeras casas em linhas retas');
+    break;
+  case 'bispo':
+    console.log('Anda inúmeras casas em diagonais');
+    break;
+  case 'cavalo':
+    console.log('Anda uma quantidade de casas suficiente para formar um L');
+    break;
+  case 'peão':
+    console.log('Anda uma casa para frente; No primeiro movimento pode andar duas casas para a frente; Captura peças apenas pela posição diagonal.')
+    break;
+  default:
+    console.log('Acho que isso não é uma peça de xadrez...')
 }
