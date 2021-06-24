9e138e0cd2ac13b1ce2f37f1158860dc018e4065
Denis Espanhol <denisgfespanhol@gmail.com>
2021-06-24 12:04:30 -0300

    Retorna o lucro de um produto vendido mil vezes.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/programa14.js
@@ -0,0 +1,8 @@
+// Escreva um programa que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+
+let productPrice = 600;
+let sellingPrice = 950;
+
+let lucre = (sellingPrice - productPrice) * 1000;
+
+console.log(lucre);
