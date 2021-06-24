dbf867969cea8268b11e80a2096aee9eb9e77462
Denis Espanhol <denisgfespanhol@gmail.com>
2021-06-24 11:59:11 -0300

    Retorna se um dentre três valores é ímpar.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/programa13.js
@@ -0,0 +1,12 @@
+// Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false.
+
+let number1 = 96;
+let number2 = 1;
+let number3 = 88;
+
+if ((number1 % 2) === 1 || (number2 % 2) === 1 || (number3 % 2) === 1) {
+  console.log(true);
+}
+else {
+  console.log(false);
+}
