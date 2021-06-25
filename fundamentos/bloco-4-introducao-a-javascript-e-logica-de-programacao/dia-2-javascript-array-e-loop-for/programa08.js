13d40b6bef31703ded84b48918814a2e8daa4600
Denis Espanhol <denisgfespanhol@gmail.com>
2021-06-25 11:11:15 -0300

    Utilizando for, cria um array do 1 ao 25

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/programa08.js
@@ -0,0 +1,9 @@
+// Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado
+
+let array = []
+
+for (array = [1]; array.length < 25; array.push(array.length + 1)) {
+  
+}
+
+console.log(array);
