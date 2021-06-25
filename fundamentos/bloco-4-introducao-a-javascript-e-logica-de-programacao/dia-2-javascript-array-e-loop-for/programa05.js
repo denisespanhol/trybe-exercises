3ad0ceab5e2f1f5febd32a30b36a7b03f5d97350
Denis Espanhol <denisgfespanhol@gmail.com>
2021-06-25 10:16:08 -0300

    Utilizando o for, descobre o maior número do array

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/programa05.js
@@ -0,0 +1,12 @@
+// Utilizando for , descubra qual o maior valor contido no array e imprima-o
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let biggerNumber = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > biggerNumber) {
+    biggerNumber = numbers[index];
+  }
+}
+
+console.log(biggerNumber);
