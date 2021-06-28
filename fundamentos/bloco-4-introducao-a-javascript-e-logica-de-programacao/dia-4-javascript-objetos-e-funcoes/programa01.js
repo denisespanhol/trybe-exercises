881780c71a17b0d0790214c1d3a760cd8e01dc80
Denis Espanhol <denisgfespanhol@gmail.com>
2021-06-28 15:51:51 -0300

    Imprime boas vindas para a personagem do objeto.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/programa01.js
@@ -0,0 +1,9 @@
+// Imprima no console uma mensagem de boas-vindas para a personagem abaixo, incluindo seu nome.
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+console.log('Bem-vinda, ' + info.personagem);
