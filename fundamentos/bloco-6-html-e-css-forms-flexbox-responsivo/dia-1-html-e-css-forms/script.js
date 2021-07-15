7b6104f476fa2f09ae2b9ab46bf8a8831567153d
Denis Espanhol <denisgfespanhol@gmail.com>
2021-07-15 14:22:03 -0300

    Exercícios do bloco 6.1

--- fundamentos/bloco-6-html-e-css-forms-flexbox-responsivo/dia-1-html-e-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-responsivo/dia-1-html-e-css-forms/script.js
@@ -2,7 +2,7 @@ const states = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG'
 
 const takeInitialsStates = document.querySelector('#sigles-states');
 for (index = 0; index < states.length; index += 1) {
-    takeInitialsStates.appendChild(document.createElement('option')).innerText = states[index];
+  takeInitialsStates.appendChild(document.createElement('option')).innerText = states[index];
 }
 
 const takeDate = document.querySelector('#date');
@@ -11,8 +11,7 @@ const takeSendButton = document.querySelector('#send-button');
 function dateVerify() {
   if (takeDate.value.length > 10) {
     window.alert('O número de caracteres da data de início é maior que o permitido');
-    break;
   }
 }
 
-takeSendButton.addEventListener('click', dateVerify);
+takeDate.addEventListener('input', dateVerify);
