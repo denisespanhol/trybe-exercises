9855aa909e10b169ca71829a8c0d65664d93ff4b
Denis Espanhol <denisgfespanhol@gmail.com>
2021-07-28 22:42:31 -0300

    Exercícios do bloco 5.4

--- fundamentos/bloco-5-javascript:-DOM-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript:-DOM-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -15,17 +15,17 @@ document.onchange = saveBackground;
 function changeColors() {
   let siteColors = document.getElementById('site-colors').value;
 
-  if(localStorage.getItem('backgroundColor', siteColors) === 'purple') {
+  if(localStorage.getItem('backgroundColor', siteColors) === 'rgb(189, 178, 255)') {
     document.body.style.backgroundColor = 'rgb(189, 178, 255)';
     document.body.style.color = 'black';
   }
   if(localStorage.getItem('backgroundColor', siteColors) === 'black') {
     document.body.style.backgroundColor = 'black';
   }
-  if(localStorage.getItem('backgroundColor', siteColors) === 'blue') {
+  if(localStorage.getItem('backgroundColor', siteColors) === 'rgb(155, 246, 255)') {
     document.body.style.backgroundColor = 'rgb(155, 246, 255)';
   }
-  if(localStorage.getItem('backgroundColor', siteColors) === 'white') {
+  if(localStorage.getItem('backgroundColor', siteColors) === 'whitesmoke') {
     document.body.style.backgroundColor = 'whitesmoke';
   }
 }
