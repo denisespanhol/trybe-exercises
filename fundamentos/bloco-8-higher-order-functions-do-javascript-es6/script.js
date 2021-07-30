20a046b838eee58b9b95801aabbfa6f472f8bb5d
Denis Espanhol <denisgfespanhol@gmail.com>
2021-07-29 21:31:25 -0300

    Exercícios do bloco 8.1

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/script.js
@@ -17,4 +17,50 @@ const newEmployees = (callback) => {
 	return employees;
 };
 
-console.table(newEmployees(employer));
+// console.log(newEmployees(employer));
+
+// 2. Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const random = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const draw = (betNum, callback) => {
+  const chooseNumber = callback(1, 6);
+  if (betNum === chooseNumber) {
+    return `Seu número de aposta é: ${betNum}.
+    O número sorteado é: ${chooseNumber}.
+    Parabéns! Você ganhou!!!`
+  }
+  return `Seu número de aposta é: ${betNum}.
+  O número sorteado é: ${chooseNumber}.
+  Ah, sinto muito! Tente novamente!`
+}
+
+// console.log(draw(4, random));
+
+// 3. Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const templateCheck = (rTemp, sTemp) => {
+  let count = 0;
+  for (let index = 0; index < rTemp.length; index += 1) {
+    if (rTemp[index] === sTemp[index]) {
+      count += 1;
+    } else if (sTemp[index] === 'N.A') {
+      count += 0;
+    } else {
+      count -= 0.5;
+    }
+  }
+  return count;
+}
+
+const testsResult = (rTemp, sTemp, callback) => `O total de pontos do estudante no teste é de: ${callback(rTemp, sTemp)}`;
+
+console.log(testsResult(RIGHT_ANSWERS, STUDENT_ANSWERS, templateCheck));
