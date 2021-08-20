1fa6a750389edb92ed0bb97139dab0303321cbd1
Denis Espanhol <denisgfespanhol@gmail.com>
2021-08-20 16:00:01 -0300

    Implementa testes para igualdade entre objetos.

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/exercise5.js
@@ -0,0 +1,18 @@
+// 5. Compare dois objetos para verificar se são idênticos ou não
+
+const obj1 = {
+  title: 'My Title',
+  description: 'My Description',
+};
+
+const obj2 = {
+  description: 'My Description',
+  title: 'My Title',
+};
+
+const obj3 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
+
+console.log(obj1 === obj2)
