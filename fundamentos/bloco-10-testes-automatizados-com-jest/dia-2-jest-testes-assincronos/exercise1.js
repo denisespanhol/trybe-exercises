b7fb253d92efbb01952f0405dcd578cae81f512c
Denis Espanhol <denisgfespanhol@gmail.com>
2021-08-23 13:48:26 -0300

    Implementação de teste assíncrono com done.

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/exercise1.js
@@ -0,0 +1,9 @@
+// 1. Escreva um teste que verifique a chamada do callback de uma função uppercase , que transforma as letras de uma palavra em letras maiúsculas. Lembre-se de ter cuidado com os falso-positivos em testes assíncronos.
+
+const uppercase =  (str, callback) => {
+  setTimeout(() => {
+    callback(str.toUpperCase());
+  }, 500);
+};
+
+module.exports = uppercase;
